0b5596d74d0c6f78cdd4eca14639811bd7135725
Karrynana <1443329043@qq.com>
2021-04-19 23:59:30 +0800

    feat: logo

--- packages/backstage/src/components/Header.vue
+++ packages/backstage/src/components/Header.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="header">
-    <h1 @click="$router.push({ name: 'Home' })">DSNAME 管理后台</h1>
-    <div>
-      <span>欢迎你，{{}}</span>
-      <el-link>退出</el-link>
-    </div>
-  </div>
+  <el-row class="header" type="flex" align="middle">
+    <el-col :span="18" @click="$router.push({ name: 'Home' })">
+      <img src="@/assets/logo.png" alt="Logo" />
+    </el-col>
+    <el-col :span="4">
+      <el-link icon="el-icon-user-solid"  type="primary">欢迎你，{{ role }} {{ name }}</el-link>
+    </el-col>
+    <el-col :span="2">
+      <el-link icon="el-icon-close" type="info">退出</el-link>
+    </el-col>
+  </el-row>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      role: "角色",
+      name: "姓名",
+    };
+  },
+};
+</script>
+
 <style lang="stylus" scoped>
-.header {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+img {
+  max-height: 3rem;
 }
 </style>
